9751e87a44c3625447808f9342f30d507a9e940a
Kodand Ram <movva.k@northeastern.edu>
2023-10-19 17:31:34 -0400

    edi

--- index.ts
+++ index.ts
@@ -1,200 +1,204 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
-import * as ip from "ip";
-const cfg = new pulumi.Config("aws-deploy");
+import * as ipModule from "ip";
 
-const vpcCfgName = cfg.require("vpcConfigName");
-const vpcRange = cfg.require("vpcRange");
-const vpcTenancy = cfg.require("vpcTenancyType");
-const gatewayCfgName = cfg.require("gatewayConfigName");
-const gatewayAttachCfgName = cfg.require("gatewayAttachConfigName");
-const publicRTCfgName = cfg.require("publicRTConfigName");
-const privateRTCfgName = cfg.require("privateRTConfigName");
-const azLimit = cfg.getNumber("azLimit");
-const subnetBits = cfg.getNumber("subnetBits");
+const projectConfig = new pulumi.Config("proj-aws-setup");
 
-const publicRouteCfgName = cfg.require("publicRouteConfigName");
-const publicDestinationRange = cfg.require("publicDestinationRange");
-const publicSubnetPrefixCfg = cfg.require("publicSubnetConfigPrefix");
-const privateSubnetPrefixCfg = cfg.require("privateSubnetConfigPrefix");
-const publicRTSubnetAssocPrefixCfg = cfg.require("publicRTSubnetAssocConfigPrefix");
-const privateRTSubnetAssocPrefixCfg = cfg.require("privateRTSubnetAssocConfigPrefix");
+console.log(projectConfig);
 
-const sgDescription = cfg.require("sgDesc");
-const sgName = cfg.require("sgConfigName");
-const ingressPortsAllowed = cfg.require("ingressPortsAllowed").split(",");
-const ingressCIDRsAllowed = cfg.require("ingressCIDRsAllowed").split(",");
+const primaryVpcName = projectConfig.require("primary_vpc_name");
+const primaryVpcCidr = projectConfig.require("primary_vpc_cidr");
+const primaryVpcInstanceTenancy = projectConfig.require("primary_vpc_instance_tenancy");
+const gatewayName = projectConfig.require("gateway_name");
+const gatewayAttachName = projectConfig.require("gateway_attach_name");
+const pubRouteTableName = projectConfig.require("pub_route_table_name");
+const privRouteTableName = projectConfig.require("priv_route_table_name");
+const maxZones = projectConfig.getNumber("max_zones");
+const subnetBits = projectConfig.getNumber("subnet_bits");
 
-const vpc = new aws.ec2.Vpc(vpcCfgName, {
-    cidrBlock: vpcRange,
-    instanceTenancy: vpcTenancy,
+const mainRouteName = projectConfig.require("main_route_name");
+const mainDestCidr = projectConfig.require("main_dest_cidr");
+const pubSubnetsPrefix = projectConfig.require("pub_subnets_prefix");
+const privSubnetsPrefix = projectConfig.require("priv_subnets_prefix");
+const pubRtSubnetsAssocPrefix = projectConfig.require("pub_rt_subnets_assoc_prefix");
+const privRtSubnetsAssocPrefix = projectConfig.require("priv_rt_subnets_assoc_prefix");
+
+const sgDescription = projectConfig.require("sgDescription");
+const sgName = projectConfig.require("sgName");
+const allowedPorts = projectConfig.require("allowedPorts").split(",");
+const allowedCIDRs = projectConfig.require("allowedCIDRs").split(",");
+
+// VPC Creation
+const primaryVpc = new aws.ec2.Vpc(primaryVpcName, {
+    cidrBlock: primaryVpcCidr,
+    instanceTenancy: primaryVpcInstanceTenancy,
     tags: {
-        Name: vpcCfgName,
+        Name: primaryVpcName,
     },
 });
 
-const ingressRulesTransformed = ingressPortsAllowed.map(port => ({
+const ingressConfig = allowedPorts.map(port => ({
     protocol: "tcp",
     fromPort: parseInt(port, 10),
     toPort: parseInt(port, 10),
-    cidrBlocks: ingressCIDRsAllowed,
+    cidrBlocks: allowedCIDRs,
 }));
 
-const applicationSG = new aws.ec2.SecurityGroup(sgName, {
-    vpcId: vpc.id,
+const appSg = new aws.ec2.SecurityGroup(sgName, {
+    vpcId: primaryVpc.id,
     description: sgDescription,
     tags: {
         Name: sgName,
     },
-    ingress: ingressRulesTransformed,
+    ingress: ingressConfig,
 });
 
-const instanceCfgType = cfg.require("instanceConfigType");
-const imgId = cfg.require("imgConfigId");
-const keyCfgName = cfg.require("keyConfigName");
-const volumeCfgSize = cfg.getNumber("volumeConfigSize");
-const volumeCfgType = cfg.require("volumeConfigType");
-const terminateOnDelete = cfg.getBoolean("terminateOnDeleteFlag");
-const ec2CfgName = cfg.require("ec2ConfigName");
+const ec2Config = {
+    type: projectConfig.require("instanceType"),
+    ami: projectConfig.require("imageId"),
+    key: projectConfig.require("keyName"),
+    volumeSize: projectConfig.getNumber("volumeSize"),
+    volumeType: projectConfig.require("volumeType"),
+    onDelete: projectConfig.getBoolean("deleteOnTermination"),
+    ec2TagName: projectConfig.require("ec2TagName"),
+};
 
-async function deploy() {
+async function setupInfrastructure() {
     try {
         const availableZones = await aws.getAvailabilityZones();
-        const zonesToUse = availableZones.names.slice(0, azLimit!);
-        const totalSubnetsCount = zonesToUse.length * 2;
-        const subnetsRanges = deriveCIDRSubnets(vpcRange, totalSubnetsCount, subnetBits!);
+        const zones = availableZones.names.slice(0, maxZones!);
+        const totalSubnets = zones.length * 2;
+        const subnetRanges = getSubnetRanges(primaryVpcCidr, totalSubnets, subnetBits!);
 
-        if (subnetsRanges instanceof Error) {
-            throw new pulumi.RunError("Failed to derive subnet CIDRs: " + subnetsRanges.message);
+        if (subnetRanges instanceof Error) {
+            throw new pulumi.RunError("Failed to determine subnet ranges: " + subnetRanges.message);
         }
 
-        let publicNetArray: aws.ec2.Subnet[] = [];
-        let privateNetArray: aws.ec2.Subnet[] = [];
+        let pubSubnets: aws.ec2.Subnet[] = [];
+        let privSubnets: aws.ec2.Subnet[] = [];
 
-        const netGateway = new aws.ec2.InternetGateway(gatewayCfgName, {
+        const internetGw = new aws.ec2.InternetGateway(gatewayName, {
             tags: {
-                Name: gatewayCfgName,
+                Name: gatewayName,
             },
         });
 
-        const gwAttach = new aws.ec2.InternetGatewayAttachment(gatewayAttachCfgName, {
-            vpcId: vpc.id,
-            internetGatewayId: netGateway.id,
+        const igAttach = new aws.ec2.InternetGatewayAttachment(gatewayAttachName, {
+            vpcId: primaryVpc.id,
+            internetGatewayId: internetGw.id,
         });
 
-        const publicRT = new aws.ec2.RouteTable(publicRTCfgName, {
-            vpcId: vpc.id,
+        const publicRt = new aws.ec2.RouteTable(pubRouteTableName, {
+            vpcId: primaryVpc.id,
             tags: {
-                Name: publicRTCfgName,
+                Name: pubRouteTableName,
             },
         });
 
-        const publicRoute = new aws.ec2.Route(publicRouteCfgName, {
-            routeTableId: publicRT.id,
-            destinationCidrBlock: publicDestinationRange,
-            gatewayId: netGateway.id,
+        const publicRoute = new aws.ec2.Route(mainRouteName, {
+            routeTableId: publicRt.id,
+            destinationCidrBlock: mainDestCidr,
+            gatewayId: internetGw.id,
         });
 
-        const privateRT = new aws.ec2.RouteTable(privateRTCfgName, {
-            vpcId: vpc.id,
+        const privateRt = new aws.ec2.RouteTable(privRouteTableName, {
+            vpcId: primaryVpc.id,
             tags: {
-                Name: privateRTCfgName,
+                Name: privRouteTableName,
             },
         });
 
-        zonesToUse.forEach((zone, idx) => {
-            const publicSubnet = new aws.ec2.Subnet(`${publicSubnetPrefixCfg}-${idx}`, {
-                vpcId: vpc.id,
+        zones.forEach((zone, idx) => {
+            const pubSubnet = new aws.ec2.Subnet(`${pubSubnetsPrefix}-${idx}`, {
+                vpcId: primaryVpc.id,
                 availabilityZone: zone,
-                cidrBlock: subnetsRanges[idx],
+                cidrBlock: subnetRanges[idx],
                 mapPublicIpOnLaunch: true,
                 tags: {
-                    Name: `${publicSubnetPrefixCfg}-${idx}`,
+                    Name: `${pubSubnetsPrefix}-${idx}`,
                 },
             });
 
-            publicNetArray.push(publicSubnet);
+            pubSubnets.push(pubSubnet);
 
-            const privateSubnet = new aws.ec2.Subnet(`${privateSubnetPrefixCfg}-${idx}`, {
-                vpcId: vpc.id,
+            const privSubnet = new aws.ec2.Subnet(`${privSubnetsPrefix}-${idx}`, {
+                vpcId: primaryVpc.id,
                 availabilityZone: zone,
-                cidrBlock: subnetsRanges[zonesToUse.length + idx],
+                cidrBlock: subnetRanges[zones.length + idx],
                 tags: {
-                    Name: `${privateSubnetPrefixCfg}-${idx}`,
+                    Name: `${privSubnetsPrefix}-${idx}`,
                 },
             });
 
-            privateNetArray.push(privateSubnet);
+            privSubnets.push(privSubnet);
         });
 
-        publicNetArray.forEach((subnet, idx) => {
-            new aws.ec2.RouteTableAssociation(`${publicRTSubnetAssocPrefixCfg}-${idx}`, {
+        pubSubnets.forEach((subnet, idx) => {
+            new aws.ec2.RouteTableAssociation(`${pubRtSubnetsAssocPrefix}-${idx}`, {
                 subnetId: subnet.id,
-                routeTableId: publicRT.id,
+                routeTableId: publicRt.id,
             });
         });
 
-        privateNetArray.forEach((subnet, idx) => {
-            new aws.ec2.RouteTableAssociation(`${privateRTSubnetAssocPrefixCfg}-${idx}`, {
+        privSubnets.forEach((subnet, idx) => {
+            new aws.ec2.RouteTableAssociation(`${privRtSubnetsAssocPrefix}-${idx}`, {
                 subnetId: subnet.id,
-                routeTableId: privateRT.id,
+                routeTableId: privateRt.id,
             });
         });
 
-        const ec2InstanceDeploy = new aws.ec2.Instance(ec2CfgName, {
-            instanceType: instanceCfgType,
-            ami: imgId,
-            keyName: keyCfgName,
-            subnetId: publicNetArray[0]?.id,
-            vpcSecurityGroupIds: [applicationSG.id],
-            disableApiTermination: cfg.getBoolean("apiTerminationFlag"),
+        const ec2Instance = new aws.ec2.Instance(ec2Config.ec2TagName, {
+            instanceType: ec2Config.type,
+            ami: ec2Config.ami,
+            keyName: ec2Config.key,
+            subnetId: pubSubnets[0]?.id,
+            vpcSecurityGroupIds: [appSg.id],
+            disableApiTermination: projectConfig.getBoolean("disableApiTerm"),
             rootBlockDevice: {
-                volumeSize: volumeCfgSize!,
-                volumeType: volumeCfgType,
-                deleteOnTermination: terminateOnDelete!,
+                volumeSize: ec2Config.volumeSize!,
+                volumeType: ec2Config.volumeType,
+                deleteOnTermination: ec2Config.onDelete!,
             },
             tags: {
-                Name: ec2CfgName,
+                Name: ec2Config.ec2TagName,
             },
         });
 
     } catch (error) {
-        console.error("Deployment Error:", error);
+        console.error("An error occurred:", error);
     }
 }
 
-// CIDR Calculation function
-function deriveCIDRSubnets(parentCIDR: string, numSubnets: number, bitsToMask: number): string[] | Error {
+function getSubnetRanges(cidr: string, subnetsCount: number, bits: number): string[] | Error {
     try {
-        if (bitsToMask > 32) {
-            throw new Error("Bits to mask exceeds the available bits in the parent CIDR");
+        if (bits > 32) {
+            throw new Error("Bits provided exceeds the limit of the CIDR notation.");
         }
 
-        function ipToInt(ip: string): number {
+        function convertIpToInt(ip: string): number {
             return ip.split('.').reduce((acc, octet) => (acc << 8) + parseInt(octet, 10), 0) >>> 0;
         }
 
-        function intToIp(int: number): string {
+        function convertIntToIp(int: number): string {
             return [(int >>> 24) & 0xFF, (int >>> 16) & 0xFF, (int >>> 8) & 0xFF, int & 0xFF].join('.');
         }
 
-        const subnetSize = 1 << (32 - bitsToMask);
-        const ipRange = ip.cidrSubnet(parentCIDR);
-        let baseIpInt = ipToInt(ipRange.networkAddress);
+        const subnetMask = 1 << (32 - bits);
+        const ipRange = ipModule.cidrSubnet(cidr);
+        let baseIp = convertIpToInt(ipRange.networkAddress);
 
-        const subnets: string[] = [];
+        const subnetList: string[] = [];
 
-        for (let i = 0; i < numSubnets; i++) {
-            const subnetCIDR = intToIp(baseIpInt) + "/" + bitsToMask;
-            subnets.push(subnetCIDR);
-            baseIpInt += subnetSize;
+        for (let i = 0; i < subnetsCount; i++) {
+            const subnetRange = convertIntToIp(baseIp) + "/" + bits;
+            subnetList.push(subnetRange);
+            baseIp += subnetMask;
         }
 
-        return subnets;
+        return subnetList;
     } catch (error) {
         return error as Error;
     }
 }
 
-
-deploy()
+setupInfrastructure();
